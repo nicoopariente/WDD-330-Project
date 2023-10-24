581cb934ed123bd1fde19e23cdcddab6a89a34d4
nicoopariente <hnpf2019@gmail.com>
2023-10-23 21:32:00 -0300

    team6-finish

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -3,101 +3,103 @@ import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 loadHeaderFooter();
 
 function getValuesOfForm() {
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
-    const street = document.getElementById('street').value;
-    const city = document.getElementById('city').value;
-    const state = document.getElementById('state').value;
-    const zipcode = document.getElementById('zipcode').value;
-    const cardNumber = document.getElementById('cardNumber').value;
-    const expiration = document.getElementById('expiration').value;
-    const securityCode = document.getElementById('securityCode').value;
+  const firstName = document.getElementById("firstName").value;
+  const lastName = document.getElementById("lastName").value;
+  const street = document.getElementById("street").value;
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+  const zipcode = document.getElementById("zipcode").value;
+  const cardNumber = document.getElementById("cardNumber").value;
+  const expiration = document.getElementById("expiration").value;
+  const securityCode = document.getElementById("securityCode").value;
 
-
-
-
-    return { firstName, lastName, street, state, city, zipcode, cardNumber, securityCode, expiration };
+  return {
+    firstName,
+    lastName,
+    street,
+    state,
+    city,
+    zipcode,
+    cardNumber,
+    securityCode,
+    expiration,
+  };
 }
 
-document.getElementById('form').addEventListener("submit", onSubmit);
-
+document.getElementById("form").addEventListener("submit", onSubmit);
 
 function onSubmit(event) {
-    event.preventDefault();
-    const formValues = getValuesOfForm();
-    const numbers = loadAndReturnTotalValues();
-    console.log(formValues);
-    console.log(numbers);
-    // checkout api.
-    //{ 
-    //    orderDate: '2021-01-27T18:18:26.095Z',
-    //    fname: "John",
-    //    lname: "Doe",
-    //    street: "123 Main",
-    //    city: "Rexburg",
-    //    state: "ID",
-    //    zip: "83440",
-    //    cardNumber: "1234123412341234",
-   //     expiration: "8/21",
-   //     code: "123",
-   //     items: [{
-   //       id: "20CXG"
-   //       name: "The North Face Pivoter 27 L Backpack"
-   //       price: 39.99,
-   //       quantity: 1
-   //     }, {
-   //       id: "14GVF",
-   //       name: "Marmot 5°F Rampart Down Sleeping Bag - 650 Fill, Mummy (For Men and Women)",
-   //       price: 229.99,
-   //       quantity: 1
-   //     }],
-   //     orderTotal: "298.18",
-   //     shipping: 12,
-   //     tax: "16.20"
-   //   }
+  event.preventDefault();
+  const formValues = getValuesOfForm();
+  const numbers = loadAndReturnTotalValues();
+  let items = [];
+  for (let i = 0; i < numbers.savedProducts.length; i++) {
+    let productItem = {
+      id: numbers.savedProducts[i].id,
+      name: numbers.savedProducts[i].Brand.Name,
+      price: numbers.savedProducts[i].FinalPrice,
+      quantity: numbers.savedProducts[i].Qty,
+    };
+    items.push(productItem);
+  }
 
-    makeCheckout()
-    
+  let date = new Date();
+  // checkout api.
+  const apiCall = {
+    orderDate: date,
+    fname: formValues.firstName,
+    lname: formValues.lastName,
+    street: formValues.street,
+    city: formValues.city,
+    state: formValues.state,
+    zip: formValues.zipcode,
+    cardNumber: formValues.cardNumber,
+    expiration: formValues.expiration,
+    code: formValues.securityCode,
+    items: items,
+    orderTotal: numbers.total,
+    shipping: numbers.shipping,
+    tax: numbers.tax,
+  };
+
+  makeCheckout(apiCall);
+}
+function total(item) {
+  let totalValue = 0;
+
+  let numberOfItems = item.length;
+  for (let i = 0; i < numberOfItems; i++) {
+    totalValue = totalValue + item[i].FinalPrice * item[i].Qty;
+  }
+  return totalValue;
 }
 
 function loadAndReturnTotalValues() {
-    const savedProducts = getLocalStorage("so-cart");
-    
-    const subtotal = total(savedProducts);
-    const tax = subtotal * 0.06;
-    const shipping = (2 * savedProducts.length) + 8;
-    const total = (subtotal + tax + shipping).toFixed(2);
-    document.getElementById('subtotal').innerHTML = total(savedProducts);
-    document.getElementById('tax').innerHTML = tax.toFixed(2);
-    document.getElementById('shipping').innerHTML = shipping;
+  const savedProducts = getLocalStorage("so-cart");
 
-    document.getElementById('total').innerHTML = total;
+  const subtotal = total(savedProducts);
+  const tax = subtotal * 0.06;
+  const shipping = 2 * savedProducts.length + 8;
+  const totalValue = (subtotal + tax + shipping).toFixed(2);
+  document.getElementById("subtotal").innerHTML = total(savedProducts);
+  document.getElementById("tax").innerHTML = tax.toFixed(2);
+  document.getElementById("shipping").innerHTML = shipping;
 
+  document.getElementById("total").innerHTML = totalValue;
 
-    return { savedProducts, subtotal, tax, shipping, total };
+  return { savedProducts, subtotal, tax, shipping, total };
 }
 
 function makeCheckout(payload) {
-    const options = {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(payload)
-      }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
 
-      fetch('http://server-nodejs.cit.byui.edu:3000/checkout', options);
-}
-
-
-function total(item) {
-    let totalValue = 0;
-
-    let numberOfItems = item.length;
-    for (let i = 0; i < numberOfItems; i++) {
-        totalValue = totalValue + item[i].FinalPrice * item[i].Qty;
-    }
-    return totalValue;
+  fetch("http://server-nodejs.cit.byui.edu:3000/checkout", options);
 }
 
 loadAndReturnTotalValues();
