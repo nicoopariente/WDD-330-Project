c0c869979b007ebe0b0f46da3a8fbc49244020ed
nicoopariente <hnpf2019@gmail.com>
2023-12-14 01:36:55 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
-        checkout: resolve(__dirname, "src/checkout/index.html"),
+        checkout: resolve(__dirname, "src/checkout/success.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         products: resolve(__dirname, "src/product-list/index.html"),
       },
